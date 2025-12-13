350ad605499b0a4f27527ae47f9dcc20dabffe84
tanvi183 <mneshat7@gmail.com>
2025-12-13 03:44:10 -0800

    project setup and installed necessary dependencies

---
+++ src/routes/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>local chef bazaar client</h1>,
+  },
+]);
+
+export default router;
